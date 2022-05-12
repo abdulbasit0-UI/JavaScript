1969198355fb66586ab4fab7b02b78b09f2f7bf8
Abdulbasit <habdulbasit099@gmail.com>
2022-05-12 21:03:08 +0200

    latest commit

--- task-app/script.js
+++ task-app/script.js
@@ -1,9 +1,14 @@
 let taskItems = [];
-
+localStorage.setItem("taskItemsRef", JSON.stringify(taskItems));
 function renderTask(task) {
   const tasks = document.querySelector(".tasks");
   const taskItem = document.querySelector(`[data-key='${task.id}']`);
   let isCompleted = null;
+
+  if (task.deleted) {
+    taskItem.remove();
+  }
+
   const node = document.createElement("div");
 
   node.setAttribute("class", `card-box card`);
@@ -32,6 +37,16 @@ function renderTask(task) {
   } else {
     tasks.append(node);
   }
+}
+
+if (taskItems.length < 0) {
+  const taskEmpty = document.querySelector(".task-empty");
+
+  taskEmpty.innerHTML`
+      <div>
+          <h1>Start Adding tasks Now</h1>
+      </div>
+      `;
 }
 
 function addTaskItem(title, description) {
@@ -88,11 +103,61 @@ function completeTask(key) {
   renderTask(taskItems[index]);
 }
 
+function deleteTask(key) {
+  const index = taskItems.findIndex((item) => item.id === Number(key));
+  const task = {
+    deleted: true,
+    ...taskItems[index],
+  };
+  taskItems.filter((item) => item.id !== Number(key));
+  renderTask(task);
+}
+
 const tasks = document.querySelector(".tasks");
 
 tasks.addEventListener("click", (event) => {
   if (event.target.classList.contains("update-btn")) {
     const taskKey = event.target.parentElement.parentElement.dataset.key;
     completeTask(taskKey);
+  }
+
+  if (event.target.classList.contains("delete-btn")) {
+    const taskKey = event.target.parentElement.parentElement.dataset.key;
+    deleteTask(taskKey);
+  }
+});
+
+const le = document.querySelector(".tasks");
+const sortable = new Sortable.create(le, {
+  group: "shared", // set both lists to same group
+  animation: 150,
+  sort: false,
+  multiDrag: true, // Enable multi-drag
+  selectedClass: "selected", // The class applied to the selected items
+  fallbackTolerance: 3, // So that we can select items on mobile
+});
+
+const completedTasks = document.querySelector(".completed-tasks");
+
+completedTasks.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-btn")) {
+    const taskKey = event.target.parentElement.parentElement.dataset.key;
+    deleteTask(taskKey);
+  }
+});
+
+const completedTasksList = new Sortable.create(completedTasks, {
+  group: "shared", // set both lists to same group
+  animation: 150,
+  sort: false,
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ref = localStorage.getItem("taskItemsRef");
+  if (ref) {
+    taskItems = JSON.parse(ref);
+    taskItems.forEach((item) => {
+      renderTask(item);
+    });
   }
 });
